7afeda84c3b2181bf7fbd2532f9c596e1c72b269
Valerate <kassyesp@hotmail.com>
2016-04-25 22:48:58 +0200

    fix

--- prototypes/liquid.lua
+++ prototypes/liquid.lua
@@ -7,7 +7,7 @@ data:extend({
     heat_capacity = "1KJ",
     base_color = {r=0, g=0.1, b=0.1},
     flow_color = {r=0.1, g=0.1, b=0.1},
-    icon = "__Liquid Plastic__/graphics/Liquid/liquid-plastic.png",
+    icon = "__Liquid Plastic__/graphics/liquid-plastic.png",
     order = "a[fluid]-b[liquid-plastic]",
     pressure_to_speed_ratio = 0.4,
     flow_to_energy_ratio = 0.59,
